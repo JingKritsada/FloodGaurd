b50b4a56ba314d00aa2a887fdc097d2b58ed343a
JingKritsada <jing.kritsada@gmail.com>
2026-03-18 14:43:22 +0700

    fix: update metadata permissions, add basic SSL plugin to Vite configuration for https test on network

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import path from "path";
 
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
@@ -10,8 +11,16 @@ export default defineConfig(() => {
 		server: {
 			port: 3000,
 			host: "0.0.0.0",
+			proxy: {
+				"/api": {
+					target: "http://localhost:3001",
+					// target: "http://172.20.10.8:3001",
+					// target: "https://uat.sukhothaicare.in.th:3004",
+					changeOrigin: true,
+				},
+			},
 		},
-		plugins: [react(), tailwindcss()],
+		plugins: [react(), tailwindcss(), basicSsl()],
 		resolve: {
 			alias: {
 				"@": path.resolve(__dirname, "./src"),
